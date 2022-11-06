db7d343b0732f6e162a4408d60639388b7cb8a47
Abdellah Aitouali <aaitoual@e2r8p5.1337.ma>
2022-11-07 00:11:33 +0100

    fixing a small error in make_pair

--- implementations/make_pair.hpp
+++ implementations/make_pair.hpp
@@ -6,7 +6,7 @@
 /*   By: aaitoual <aaitoual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 23:49:28 by aaitoual          #+#    #+#             */
-/*   Updated: 2022/11/06 23:52:09 by aaitoual         ###   ########.fr       */
+/*   Updated: 2022/11/07 00:11:07 by aaitoual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 
 namespace ft {
     template <class T1, class T2>  ft::pair<T1, T2> make_pair (T1 x, T2 y) {
-        return ft::make_pair<T1, T2>(x, y);
+        return ft::pair<T1, T2> (x, y);
     }
 }
